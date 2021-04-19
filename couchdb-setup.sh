4a94a72ae4c287bcc3917a57b55b9ed572f8f9af
Vincent Martinez <vincent.martinez@lifen.fr>
2021-04-19 23:24:43 +0200

    set couchdb database to public

--- couchdb-setup.sh
+++ couchdb-setup.sh
@@ -10,3 +10,9 @@ curl -X PUT http://admin:123abc@couchdb:5984/coops/_design/index \
      -d @couchdb-coops-init.json
 curl -X PUT http://admin:123abc@couchdb:5984/envelop/_design/index \
      -d @couchdb-envelop-init.json
+
+# Set databases to public to allow app to access documents
+curl -X PUT http://admin:123abc@couchdb:5984/coops/_security -d '{}'
+curl -X PUT http://admin:123abc@couchdb:5984/inventory/_security -d '{}'
+curl -X PUT http://admin:123abc@couchdb:5984/envelop/_security -d '{}'
+curl -X PUT http://admin:123abc@couchdb:5984/shopping_carts/_security -d '{}'
