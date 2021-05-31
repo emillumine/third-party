f4bd79be19142484e3800873457b0d51d7f45f27
Alexis Aoun <pizzaman@localhost.localdomain>
2021-05-31 14:08:26 +0200

    refactor: putting before script commmands in one script

---
+++ unit_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#setup de l'environnement de test
+pip install -U pip setuptools 
+pip install -r requirements.txt 
+
+#setup des fichiers paramètres
+cp outils/settings_example.py outils/settings.py  
+cp outils/settings_secret_example.py outils/settings_secret.py
+cp outils/config.example.py outils/config.py
