5cbd2534e02e9701c34e20d93e84b1b60fbfe5c1
Francois COLOMBIER <francois@cooperatic.fr>
2022-04-19 12:23:05 +0000

    Set default PREPA_ODOO_URL to /members/prepa-odoo

--- members/views.py
+++ members/views.py
@@ -103,7 +103,7 @@ def inscriptions(request, type=1):
                'show_ftop_button': getattr(settings, 'SHOW_FTOP_BUTTON', True),
                'db': settings.COUCHDB['dbs']['member'],
                'ASSOCIATE_MEMBER_SHIFT' : getattr(settings, 'ASSOCIATE_MEMBER_SHIFT', ''),
-               'prepa_odoo_url' : getattr(settings, 'PREPA_ODOO_URL', ''),
+               'prepa_odoo_url' : getattr(settings, 'PREPA_ODOO_URL', '/members/prepa-odoo'),
                }
 
     response = HttpResponse(template.render(context, request))
