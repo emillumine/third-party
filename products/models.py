285f694e4d079077ab12ff3f3ffe8d3419db212c
Damien Moulard <dam.moulard@gmail.com>
2022-06-24 10:45:17 +0200

    ALC: assign computed qty to supplier with highest priority

--- products/models.py
+++ products/models.py
@@ -607,7 +607,7 @@ class CagetteProducts(models.Model):
 
             if supplier_ids is not None and len(supplier_ids) > 0:
                 # Get products/supplier relation
-                f = ["id", "product_tmpl_id", 'date_start', 'date_end', 'package_qty', 'price', 'name', 'product_code']
+                f = ["id", "product_tmpl_id", 'date_start', 'date_end', 'package_qty', 'price', 'name', 'product_code', 'sequence']
                 c = [['name', 'in', [ int(x) for x in supplier_ids]]]
                 psi = api.search_read('product.supplierinfo', c, f)
 
@@ -669,7 +669,8 @@ class CagetteProducts(models.Model):
                                 'supplier_id': int(psi_item["name"][0]),
                                 'package_qty': psi_item["package_qty"],
                                 'price': psi_item["price"],
-                                'product_code': psi_item["product_code"]
+                                'product_code': psi_item["product_code"],
+                                'sequence': psi_item["sequence"]
                             })
 
                 for s in sales:
