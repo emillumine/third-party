fd149d3d665330a8044c47f89287e7d176debb4b
Damien Moulard <dam.moulard@gmail.com>
2022-06-29 11:23:08 +0200

    add check in shelf admin

--- shelfs/static/js/shelfs_admin.js
+++ shelfs/static/js/shelfs_admin.js
@@ -600,6 +600,9 @@ $(document).ready(function() {
             } else if (barcode.length == 12 && barcode.indexOf('0') !== 0) {
             // User may use a scanner which remove leading 0
                 barcode = '0' + barcode;
+            } else if (barcode.length >= 8) {
+                // For EAN8
+                barcode = barcode.substring(barcode.length-8);
             }
 
 
